5678113e555444e68ff3cac133de93bfc512d36a
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2023-10-13 17:34:16 +0700

    API Service
    Update User Source
    - add login method

--- lib/source/user_source.dart
+++ lib/source/user_source.dart
@@ -21,4 +21,20 @@ class UserSource {
       return {'success': false};
     }
   }
+
+  static Future<Map> login(String username, String pasword) async {
+    String url = '${Api.user}/register.php';
+    try {
+      Response response = await Client().post(Uri.parse(url), body: {
+        'username': username,
+        'password': pasword,
+      });
+      DMethod.printTitle('User Source -  login', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody;
+    } catch (e) {
+      DMethod.printTitle('User Source - login', e.toString());
+      return {'success': false};
+    }
+  }
 }
