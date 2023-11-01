ebe6fa489b0e0902fdabc0efafef4190c660ae04
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2023-11-02 01:48:41 +0700

    Update home page

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,21 +1,74 @@
-import 'package:d_view/d_view.dart';
+import 'package:discuss_app/controller/c_home.dart';
+import 'package:discuss_app/page/fragment/account_fragment.dart';
+import 'package:discuss_app/page/fragment/explore_fragment.dart';
+import 'package:discuss_app/page/fragment/feed_fragment.dart';
+import 'package:discuss_app/page/fragment/my_topic_fragment.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  logout() {}
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: DView.appBarCenter('Home'),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => logout(),
-          child: const Text('Logout'),
+    List menu = [
+      {
+        'icon': Icons.feed,
+        'label': 'Feed',
+        'view': const FeedFragment(),
+      },
+      {
+        'icon': Icons.public,
+        'label': 'Explore',
+        'view': const ExploreFragment(),
+      },
+      {
+        'icon': Icons.library_books,
+        'label': 'Feed',
+        'view': const MyTopicFragment(),
+      },
+      {
+        'icon': Icons.account_circle,
+        'label': 'Profile',
+        'view': const AccountFragment(),
+      },
+    ];
+    return Consumer<CHome>(builder: (context, _, child) {
+      return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: menu[_.indexMenu]['view'],
         ),
-      ),
-    );
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 24),
+          child: FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: () {},
+            mini: false,
+            tooltip: 'Buat Topic Baru',
+            child: const Icon(
+              Icons.create,
+              color: Color(0xff00BBFF),
+            ),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _.indexMenu,
+          onTap: (newIndex) {
+            _.indexMenu = newIndex;
+          },
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          items: menu.map((e) {
+            return BottomNavigationBarItem(
+              icon: Icon(e['icon']),
+              label: e['label'],
+            );
+          }).toList(),
+        ),
+      );
+    });
   }
 }
